e1c9d7fa1cc008bb4a1b22c990c14fb57695ce47
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-10-22 15:22:48 +0100

    feat: add yarn run script

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+if cat package.json > /dev/null 2>&1; then
+    scripts=$(cat package.json | jq .scripts | sed '1d;$d' | fzf --height 40%)
+
+    if [[ -n $scripts ]]; then
+        script_name=$(echo $scripts | awk -F ': ' '{gsub(/"/, "", $1); print $1}')
+        echo "yarn run "$script_name;
+        yarn run $script_name
+    else
+        echo "Exit: You haven't selected any script"
+    fi
+else
+    echo "Error: There's no package.json"
+fi
