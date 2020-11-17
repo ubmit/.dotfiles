16eb6bb2b20ea8bfa4ca2c7d4bc8c02ca1c6dfb7
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-11-17 13:46:08 +0000

    fish: remove unnecessary lines from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,22 +14,13 @@ fish_vi_key_bindings
 fish_ssh_agent
 
 # fnm
-fnm env --multi --use-on-cd --shell=fish | source
+fnm env --use-on-cd --shell=fish | source
 
 # fzf ripgrep
 set -gx FZF_DEFAULT_COMMAND  'rg --files --follow --hidden'
 
 # set fish greeting
-
 set quotes "\"Obstacles don’t have to stop you. If you run into a wall, don’t turn around and give up. Figure out how to climb it, go through it, or work around it.\""\n"- Michael Jordan" "\"Don’t compare yourself with anyone in this world... if you do so, you are insulting yourself.\""\n"- Bill Gates" "\"Don't fly too close to the sun.\"" "\"A única coisa que ninguém pode roubar de você é o seu conhecimento.\""\n"- Mamãe"
-
-# set number_of_quotes 0
-# set quotes 
-
-# while read quote
-#    set number_of_quotes (math $number_of_quotes + 1)
-#    set quotes[$number_of_quotes] $quote
-# end < quotes.txt
 
 set random_quote $quotes[(random 1 (count $quotes))]
 
@@ -38,15 +29,5 @@ set fish_greeting $random_quote
 # execute starship
 starship init fish | source
 
-# set android studio paths
-set --export ANDROID_SDK_ROOT $HOME/Library/Android/sdk
-
-set PATH $ANDROID_SDK_ROOT/emulator $PATH
-set PATH $ANDROID_SDK_ROOT/tools $PATH
-set PATH $ANDROID_SDK_ROOT/tools/bin $PATH
-set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
-
-alias start-android-emulator "$ANDROID_SDK_ROOT/emulator/emulator @Pixel_3a_API_30_x86"
-
-# add $HOME/.emacs.d/bin to PATH
-set PATH $HOME/.emacs.d/bin $PATH
+# Load rbenv automatically by appending
+status --is-interactive; and source (rbenv init -|psub)
