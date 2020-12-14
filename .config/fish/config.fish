348472da06d218052d32db3c0c1ea3d8477e3019
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-12-14 22:05:10 +0000

    fish: organize commands according to their context

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,23 @@
 # load aliases
 . $HOME/.config/fish/aliases.fish
 
+# load fish_ssh_agent
+fish_ssh_agent
+
+# load starship
+starship init fish | source
+
+# load fnm
+fnm env --use-on-cd --shell=fish | source
+
+# load rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
 # add $HOME/.local/bin to PATH
 set PATH $HOME/.local/bin $PATH
 
 # add python scripts location to PATH
 set PATH $HOME/Library/Python/2.7/bin $PATH
-
-# set vi-mode as default
-fish_vi_key_bindings
-
-# load fish_ssh_agent
-fish_ssh_agent
-
-# fnm
-fnm env --use-on-cd --shell=fish | source
-
-# fzf ripgrep
-set -gx FZF_DEFAULT_COMMAND  'rg --files --follow --hidden'
-
-# execute starship
-starship init fish | source
-
-# load rbenv
-status --is-interactive; and source (rbenv init -|psub)
 
 # add man pages to PATH
 set PATH /usr/local/opt/erlang/lib/erlang/man $PATH
@@ -31,13 +25,16 @@ set PATH /usr/local/opt/erlang/lib/erlang/man $PATH
 # add cargo to PATH
 set PATH $HOME/.cargo/bin $PATH
 
-# remove greeting
+# set empty greeting
 set fish_greeting
 
-# add ruby gems to PATH
-set PATH $HOME/.rbenv/versions/*/lib/ruby/gems $PATH
+# set vi-mode as default
+fish_vi_key_bindings
 
-# automatically change kitty colors based on time of day
+# set FZF_DEFAULT_COMMAND 
+set -gx FZF_DEFAULT_COMMAND  'rg --files --follow --hidden'
+
+# change kitty colors based on time of day
 if command -v "sunshine" >/dev/null
   if [ (sunshine -s "!#Porto") = "day" ]
     light-theme
