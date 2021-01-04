de1029443aaa5124424f70ba5b4e5710e4c85163
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-01-04 19:41:19 +0000

    chore(fish): update and cleanup on config and aliases files

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,5 +1,4 @@
 # git shortcuts
-alias g git
 alias glog "git log --all --decorate --oneline --graph"
 
 # vim shortcuts
@@ -11,30 +10,17 @@ alias vimdiff "nvim -d"
 alias tmux "tmux -u" 
 alias t tmux
 
-# dotfiles magic
+# dotfiles
 alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-
-# generate .nvmrc file using the current node version
-alias nvmrc "node -v > .nvmrc"
 
 # use Sindresorhus trash instead of rm
 alias rm "trash"
-
-# remove folder recursively
-alias rmf "rm -rf"
-
-# npm
-alias ni "npm install"
-alias nd "npm run dev"
-alias nt "npm run test"
-alias ntw "nt -- --watch"
-alias ntc "nt -- --coverage"
 
 # vscode
 alias code code-insiders
 
 # yarn/npm run
-alias run "bash $HOME/scripts/run.sh"
+alias yrun "bash $HOME/scripts/yarn-run.sh"
 
-# change bat theme according based on the OS theme
-alias cat="bat --theme=(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
+# use bat instead of cat 
+alias cat "bat"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,14 +4,11 @@
 # load starship
 starship init fish | source
 
-# load fnm
-fnm env --use-on-cd --shell=fish | source
-
-# load rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
 # load fish_ssh_agent
 fish_ssh_agent
+
+# load asdf
+source ~/.asdf/asdf.fish
 
 # add $HOME/.local/bin to PATH
 set PATH $HOME/.local/bin $PATH
@@ -19,26 +16,14 @@ set PATH $HOME/.local/bin $PATH
 # add python scripts location to PATH
 set PATH $HOME/Library/Python/2.7/bin $PATH
 
-# add man pages to PATH
-set PATH /usr/local/opt/erlang/lib/erlang/man $PATH
-
-# add cargo to PATH
-set PATH $HOME/.cargo/bin $PATH
-
 # add oly to PATH
 set PATH $HOME/.oly/bin $PATH
-
-# add doom to PATH
-set PATH $HOME/.emacs.d/bin $PATH
 
 # set empty greeting
 set fish_greeting
 
-# set default key bindings
-fish_default_key_bindings
+# set vi key bindings
+fish_vi_key_bindings
 
 # set FZF_DEFAULT_COMMAND 
 set -gx FZF_DEFAULT_COMMAND  'rg --files --follow --hidden'
-
-# set default editor
-set EDITOR 'vim'
