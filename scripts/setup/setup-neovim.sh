ce9e6da35b6b4bce83a1a6aa3b1da1fccc245c3d
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2022-01-31 21:11:48 +0000

    chore(nvim-setup): remove vim-plug

--- scripts/setup/setup-neovim.sh
+++ scripts/setup/setup-neovim.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Installing vim-plug..."
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 echo "Installing Python 2 and Python 3 providers..."
 curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py
 python2 $HOME/get-pip.py
